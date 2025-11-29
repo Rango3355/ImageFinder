e45417c9414c2a513a2a8a461d4f84b2afe18c80
Rango3355 <mike.loader95@gmail.com>
2025-11-29 14:48:12 +0000

    feat(MainPage): setup to call image collector

--- ImageFinder.mau/MainPage.xaml.cs
+++ ImageFinder.mau/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ImageFinder.domain.Models;
+using ImageFinder.application.Collector;
 using ImageFinder.mau.Helper;
 
 namespace ImageFinder.mau;
@@ -7,10 +8,12 @@ public partial class MainPage : ContentPage
 {
     private readonly ImageDirectoryModel _imageDirectoryModel = new();
     private readonly DirectoryHelper _directoryHelper = new();
+    private readonly IImageCollector _imageCollector;
 
-    public MainPage()
+    public MainPage(IImageCollector imageCollector)
     {
         InitializeComponent();
+        _imageCollector = imageCollector ?? throw new ArgumentNullException(nameof(imageCollector));
     }
 
     private async void OnSourcePathClicked(object? sender, EventArgs e)
@@ -21,5 +24,26 @@ public partial class MainPage : ContentPage
     private async void OnDestinationPathClicked(object? sender, EventArgs e)
     {
         await _directoryHelper.HandlePathSelectionAsync(lblDestinationPath, path => _imageDirectoryModel.DestinationDirectoryPath = path);
+    }
+
+    private async void OnProcessImages_Clicked(object? sender, EventArgs e)
+    {
+        try
+        {
+            if(_imageDirectoryModel.SourceDirectoryPath == null || _imageDirectoryModel.DestinationDirectoryPath == null)
+            {
+                throw new NullReferenceException("Please select both source and destination directories.");
+            }
+
+            await _imageCollector.CollectAndOrganiseImagesAsync(_imageDirectoryModel);
+        }
+        catch (NullReferenceException ex)
+        {
+            await DisplayAlertAsync("Input Error", ex.Message, "OK");
+        }
+        catch (Exception)
+        {
+            await DisplayAlertAsync("Error", "Unexpected error ", "OK");
+        }
     }
 }
