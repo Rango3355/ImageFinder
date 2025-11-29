671074578569503bccf1c79eb4e8f99780385016
Rango3355 <mike.loader95@gmail.com>
2025-11-29 18:22:16 +0000

    fix: ui crash when canceling folder picker

--- ImageFinder.mau/Helper/DirectoryHelper.cs
+++ ImageFinder.mau/Helper/DirectoryHelper.cs
@@ -5,25 +5,25 @@ namespace ImageFinder.mau.Helper;
 
 public class DirectoryHelper
 {
-    public async Task HandlePathSelectionAsync(Label targetLabel, Action<string> setModelPath)
+    public async Task<bool> HandlePathSelectionAsync(Label targetLabel, Action<string> setModelPath)
     {
         FolderPickerResult? result = await GetDirectory();
 
         if (result == null || !result.IsSuccessful)
         {
-            throw new InvalidOperationException("No folder was selected.");
+            return false;
         }
 
         string path = result.Folder?.Path ?? string.Empty;
         targetLabel.Text = path;
         setModelPath(path);
         targetLabel.IsVisible = true;
+
+        return true;
     }
 
     private static async Task<FolderPickerResult?> GetDirectory()
     {
-        FolderPickerResult? directory = await FolderPicker.PickAsync(default);
-
-        return directory;
+        return await FolderPicker.PickAsync(default);
     }
 }
