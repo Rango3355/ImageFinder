3a663b010bb33461922938fad091fde0f38cdab1
Rango3355 <mike.loader95@gmail.com>
2025-11-29 13:53:54 +0000

    feat: create directory helper

---
+++ ImageFinder.mau/Helper/DirectoryHelper.cs
@@ -0,0 +1,29 @@
+﻿
+using CommunityToolkit.Maui.Storage;
+
+namespace ImageFinder.mau.Helper;
+
+public class DirectoryHelper
+{
+    public async Task HandlePathSelectionAsync(Label targetLabel, Action<string> setModelPath)
+    {
+        FolderPickerResult? result = await GetDirectory();
+
+        if (result == null || !result.IsSuccessful)
+        {
+            throw new InvalidOperationException("No folder was selected.");
+        }
+
+        string path = result.Folder?.Path ?? string.Empty;
+        targetLabel.Text = path;
+        setModelPath(path);
+        targetLabel.IsVisible = true;
+    }
+
+    private static async Task<FolderPickerResult?> GetDirectory()
+    {
+        FolderPickerResult? directory = await FolderPicker.PickAsync(default);
+
+        return directory;
+    }
+}
