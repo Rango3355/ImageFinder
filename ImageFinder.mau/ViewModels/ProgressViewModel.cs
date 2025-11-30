a6408182c20d0ae67b3916760e0a8052f7ae4fda
Rango3355 <mike.loader95@gmail.com>
2025-11-30 16:17:31 +0000

    feat: add progress page

---
+++ ImageFinder.mau/ViewModels/ProgressViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ImageFinder.application.Collector;
+using ImageFinder.domain.Models;
+
+namespace ImageFinder.mau.ViewModels;
+
+public partial class ProgressViewModel : ObservableObject
+{
+
+    private readonly IImageCollector _imageCollector;
+    private readonly ImageDirectoryModel _imageDirectoryModel;
+
+    private double _progress;
+    public double Progress
+    {
+        get => _progress;
+        set => SetProperty(ref _progress, value);
+    }
+
+    public IAsyncRelayCommand StartProcessingCommand { get; } = default!;
+
+    public ProgressViewModel(
+            IImageCollector imageCollector,
+            ImageDirectoryModel imageDirectoryModel)
+    {
+        _imageCollector = imageCollector;
+        _imageDirectoryModel = imageDirectoryModel;
+        StartProcessingCommand = new AsyncRelayCommand(StartProcessingAsync);
+    }
+
+    public async Task StartProcessingAsync()
+    {
+        Progress<double> progress = new(p => Progress = p);
+
+        await _imageCollector.CollectAndOrganiseImagesAsync(
+            _imageDirectoryModel,
+            progress);
+    }
+}
